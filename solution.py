f36f8c10abd11e03f474076fa07dec0247354e0b
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-13 17:53:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,19 +4,12 @@ from scipy.stats import t
 
 chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
-def solution(data: np.ndarray, threshold: float = 300, alpha: float = 0.1) -> bool:
-# data - массив с историческими данными
-# threshold - установленный порог затрат
-# alpha - уровень значимости критерия
-    n = len(data) # размер выборки
-    mean = np.mean(data) # выборочное среднее
-    std = np.std(data, ddof=1) # выборочное стандартное отклонение
-
-# рассчитываем t-статистику
-    t_stat = (mean - threshold) / (std / np.sqrt(n))
-
-# рассчитываем критическое значение t-статистики
-    t_crit = t.ppf(1 - alpha, n - 1)
-
-# сравниваем t-статистику с критическим значением и возвращаем ответ
-    return t_stat < t_crit
+def solution(x: np.array) -> bool:
+    alpha = 0.1
+    n = len(x)
+    mean = np.mean(x)
+    std = np.std(x, ddof=1)
+    t_stat = t.ppf(1 - alpha, n - 1)
+    left = mean - t_stat * std / np.sqrt(n)
+    right = mean + t_stat * std / np.sqrt(n)
+    return right <= 300
