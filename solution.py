c16726ab0ce0417ca7426fdaad4cacabdadbcf3c
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-13 11:28:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import t
 
+chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(data: np.ndarray, threshold: float = 300, alpha: float = 0.1) -> bool:
+# data - массив с историческими данными
+# threshold - установленный порог затрат
+# alpha - уровень значимости критерия
+    n = len(data) # размер выборки
+    mean = np.mean(data) # выборочное среднее
+    std = np.std(data, ddof=1) # выборочное стандартное отклонение
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+# рассчитываем t-статистику
+    t_stat = (mean - threshold) / (std / np.sqrt(n))
+
+# рассчитываем критическое значение t-статистики
+    t_crit = t.ppf(1 - alpha, n - 1)
+
+# сравниваем t-статистику с критическим значением и возвращаем ответ
+    return t_stat < t_crit
